4e2b81decfe8341d4f0b551a0a5167ba2528de7d
hoomanti <manisha.edu@protonmail.com>
2022-01-23 13:56:02 +0530

    calculate length of line using cartesian system

--- LineComparision.java
+++ LineComparision.java
@@ -1,5 +1,11 @@
 public class LineComparision {
 	public static void main(String args[]) {
-		System.out.println("WELCOME");
+		double x1 = 3;
+		double y1 = 4;
+		double x2 = 7;
+		double y2 = 1;
+
+		double lengthOfLine = Math.sqrt((y2 -y1) * (y2 - y1) + (x2 - x1) * (x2 - x1));
+		System.out.println(lengthOfLine);
 	}
 }
